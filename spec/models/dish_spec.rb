98caadb30dd1604c39cb5f22ec87b7c70de13ac0
Blake Sergesketter <blakesergesketter@gmail.com>
2023-11-02 20:04:08 -0500

    model method testing of total_calories.

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Dish, type: :model do
       it {should belong_to :chef}
   end
 
+  describe "#total_calories" do 
+    it "calculates a dishes total calories" do 
+      @flay = Chef.create!(name: "Bobby Flay")
+      @ramen = Dish.create!(name: "Ramen", description: "A really good soup dish", chef_id: @flay.id)
+      @noodles = Ingredient.create!(name: "Ramen noodles", calories: 10)
+      @broth = Ingredient.create!(name: "Vegetable broth", calories: 15)
+      DishIngredient.create!(dish_id: @ramen.id, ingredient_id: @noodles.id)
+      DishIngredient.create!(dish_id: @ramen.id, ingredient_id: @broth.id)
+
+      expect(@ramen.total_calories).to eq(25)
+    end
+  end
 end
