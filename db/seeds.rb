3ed2618bf05ae85eb62151e6416860c55c53e658
Joseph Lee <jhjlee702@gmail.com>
2023-11-04 15:49:39 -0600

    creat some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+@chef1 = Chef.create!(name: "Gordon Ramsey")
+    
+@dish1 = @chef1.dishes.create!(name: "Chicken Pot Pie", description: "Mom's recipe")
+
+@ingredient1 = Ingredient.create!(name: "Chicken", kcal: 200)
+@ingredient2 = Ingredient.create!(name: "Milk", kcal: 60)
+@ingredient3 = Ingredient.create!(name: "Flour", kcal: 30)
+
+@dish_ingredient_1 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient1.id)
+@dish_ingredient_2 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient2.id)
+@dish_ingredient_3 = DishIngredient.create!(dish_id: @dish1.id, ingredient_id: @ingredient3.id)
