9a5395cf3702a9f3ba79197ee0eeeb85f76446f8
Brendan Bondurant <bondurant.brendan@gmail.com>
2023-11-04 17:53:25 -0600

    US 3 complete, model testing and feature testing

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,4 +1,18 @@
 class Chef < ApplicationRecord
    validates_presence_of :name
    has_many :dishes
+
+
+   def ingredient_list
+   ingredients = []
+      self.dishes.each do |dish|
+         ingredients << dish.ingredients.pluck(:name)
+      end
+      ingredients.flatten.uniq
+   end
+
+
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/chef/:id", to: "chef#show"
+  get "/chef/:id/ingredients", to: "ingredients#index"
 
   resources :chef, only: [:show, :create] do
     resources :dishes, only: [:show, :create, :update]
   end
+  
+  resources :chef, only: [:show]
+
+  
 
 end
