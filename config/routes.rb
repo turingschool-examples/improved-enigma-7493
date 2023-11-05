3129d9beaa7cb7c4a4b56e8b77a7a03f5ea136a4
Kevin Zolman <kevin.zolman@gmail.com>
2023-11-05 08:33:57 -0700

    Refactor US2 route and form

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :dishes, only: :show
-  post "/dishes/:id", to: "dish_ingredients#create"
+  post "/dishes/:id/ingredients", to: "dishes/ingredients#create"
+  resources :chefs, only: :show
 end
