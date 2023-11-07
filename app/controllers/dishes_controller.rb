2bcb672b8a1febdf222fb67f3f86bcf345beb729
Scott DeVoss <scottdevoss@yahoo.com>
2023-11-06 17:19:48 -0700

    almost done with US 2. Says Dish must exist

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -2,4 +2,9 @@ class DishesController < ApplicationController
   def show
     @dish = Dish.find(params[:id])
   end
+
+  def update
+    DishIngredient.create!(dish_id: params[:dish_id], ingredient_id: params[:ingredient_id])
+    redirect_to "/dishes/#{params[:id]}"
+  end
 end
