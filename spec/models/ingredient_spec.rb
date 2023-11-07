13504ad5f4c49228128d13a843692f2dee8ce3c4
Xander Hendry <vhendry.coding@gmail.com>
2023-11-07 11:25:38 -0500

    add migrations and relationships for dishes - ingredients, and add spec tests for validation/relationships

---
+++ spec/models/ingredient_spec.rb
@@ -0,0 +1,13 @@
+RSpec.describe Ingredient, type: :model do
+
+  describe "validations" do
+      it {should validate_presence_of :name}
+      it {should validate_presence_of :calories}
+  end
+
+  describe "relationships" do
+      it {should have_many :dish_ingredients} 
+      it {should have_many :dishes}
+  end
+
+end
