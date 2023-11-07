70bdc1cb29acac4409bcf60f26adee98c3a5b005
Eric Belongea <eric.belongea@gmail.com>
2023-11-06 20:09:33 -0700

    Finish in two hours and fifteen mins, no extensions

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,6 +1,5 @@
 class IngredientsController < ApplicationController
   def index
     @chef = Chef.find(params[:chef_id])
-    # require 'pry'; binding.pry
   end
 end
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,12 +1,4 @@
 class Chef < ApplicationRecord
    validates_presence_of :name
    has_many :dishes
-
-   def list_ingredients
-      dishes.map do |dish|
-         dish.ingredients.select do |ingredient|
-            ingredient.name
-         end
-      end
-   end
 end
