a096c36b26386b38858278ce43944eb7c99d8cac
Sam Tran <samuelttran@gmail.com>
2023-11-07 15:25:55 -0700

    Add chef name

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -21,6 +21,8 @@ RSpec.describe "dish show", type: :feature do
     it 'They see dishes name and description and chefs name' do
       expect(page).to have_content("Vegetable Soup")
       expect(page).to have_content("Description: It has veggies")
+      expect(page).to have_content("Chef: Sam")
+
     end
 
     it 'They see a list of ingredients ant total calorie count for that dish' do
@@ -31,9 +33,3 @@ RSpec.describe "dish show", type: :feature do
   end
 
 end
-# As a visitor
-# When I visit a dish's show page
-# I see the dish’s name and description
-# And I see a list of ingredients for that dish
-# and a total calorie count for that dish
-# And I see the chef's name.
