ccad8e77ac74765f1b420bc95c20d64e8a532fca
Sooyung Kim <kimx1279@gmail.com>
2023-11-06 17:11:40 -0700

    Add extension 2

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,4 +6,8 @@ class Chef < ApplicationRecord
    def distinct_ingredients
       ingredients.distinct.order(:name).pluck(:name)
    end
+
+   def popular_ingredients
+      ingredients.select("ingredients.*, count(ingredients.id) as pop_ingredients").group("ingredients.id").order(pop_ingredients: :desc).limit(3)
+   end
 end
