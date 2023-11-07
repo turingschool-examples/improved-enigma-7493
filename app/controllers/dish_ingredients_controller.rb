19d1b6a0e03ac32cc8d0863156bd17d6e0cd2f1f
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-11-06 19:14:39 -0700

    Implement feature to satisfy US Ex 1

--- app/controllers/dish_ingredients_controller.rb
+++ app/controllers/dish_ingredients_controller.rb
@@ -4,4 +4,10 @@ class DishIngredientsController < ApplicationController
     DishIngredient.create(dish_id: params[:id], ingredient_id: params[:add_ingredient])
     redirect_to "/dishes/#{params[:id]}"
   end
+  
+  def destroy
+    DishIngredient.where(dish_id: params[:id], ingredient_id: params[:ingredient_id]).first.destroy
+    redirect_to "/dishes/#{params[:id]}"
+    
+  end
 end
