562e6d2084866b72c5219c016ac94d30629519ef
Miranda Delaney <delaney.miranda1@gmail.com>
2023-11-08 12:53:52 -0700

    feat: US 2 complete

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/dishes/:dish_id', to: 'dishes#show'
+  get '/dishes/:id', to: 'dishes#show'
+  patch '/dishes/:id', to: 'dish_ingredients#new'
 end
