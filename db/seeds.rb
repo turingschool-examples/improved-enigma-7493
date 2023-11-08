a96871a6cc60970133d7e95368b03caf024e4553
Nathan Trautenberg <Natester123@mac.com>
2023-11-08 15:37:55 -0700

    made seeds, made test for dish show page, made route, controller, and method for calorie_count.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+@chef1 = Chef.create(name: "Frenchie")
+@chef2 = Chef.create(name: "Baker Boy")
+
+@dish1 = @chef1.dishes.create(name: "Soup", description: "Soupy Soup")
+@dish2 = @chef1.dishes.create(name: "Goupy Goup", description: "Goupy Soupy Soup Goup")
+
+@ingredient1 = Ingredient.create(name: "Onion", calories: 10)
+@ingredient2 = Ingredient.create(name: "Garlic", calories: 20)
+@ingredient3 = Ingredient.create(name: "Tomato", calories: 30)
+@ingredient4 = Ingredient.create(name: "Noodle", calories: 40)
+@ingredient5 = Ingredient.create(name: "Kisses", calories: 50)
+@ingredient6 = Ingredient.create(name: "Hugs", calories: 60)
+
+DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient1.id)
+DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient2.id)
+DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient3.id)
+DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient4.id)
+DishIngredient.create(dish_id: @dish2.id, ingredient_id: @ingredient5.id)
+DishIngredient.create(dish_id: @dish2.id, ingredient_id: @ingredient6.id)
--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -5,19 +5,29 @@ RSpec.describe Dish, type: :feature do
     @chef1 = Chef.create(name: "Frenchie")
     @chef2 = Chef.create(name: "Baker Boy")
 
-    @dish1 = @chef1.dishes.create(name: "Soup", description: "Soup")
+    @dish1 = @chef1.dishes.create(name: "Soup", description: "Soupy Soup")
     @dish2 = @chef1.dishes.create(name: "Goupy Goup", description: "Goupy Soupy Soup Goup")
 
-    # @actor1 = Actor.create(name: "Nathan Turing", age: 31)
-    # @actor2 = Actor.create(name: "Thomas Hanks", age: 69)
+    @ingredient1 = Ingredient.create(name: "Onion", calories: 10)
+    @ingredient2 = Ingredient.create(name: "Garlic", calories: 20)
+    @ingredient3 = Ingredient.create(name: "Tomato", calories: 30)
+    @ingredient4 = Ingredient.create(name: "Noodle", calories: 40)
+    @ingredient5 = Ingredient.create(name: "Kisses", calories: 50)
+    @ingredient6 = Ingredient.create(name: "Hugs", calories: 60)
 
-    # MovieActor.create(movie_id: @movie3.id, actor_id: @actor1.id)
-    # MovieActor.create(movie_id: @movie1.id, actor_id: @actor1.id)
-    # MovieActor.create(movie_id: @movie2.id, actor_id: @actor1.id)
+    DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient1.id)
+    DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient2.id)
+    DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient3.id)
+    DishIngredient.create(dish_id: @dish1.id, ingredient_id: @ingredient4.id)
+    DishIngredient.create(dish_id: @dish2.id, ingredient_id: @ingredient5.id)
+    DishIngredient.create(dish_id: @dish2.id, ingredient_id: @ingredient6.id)
+
+
+    
     
   end
   describe "dish show page" do
-    xit "shows movie title, year, and genre" do
+    it "shows movie title, year, and genre" do
       # Story 1 of 3
       # As a visitor
       # When I visit a dish's show page
@@ -26,8 +36,21 @@ RSpec.describe Dish, type: :feature do
       # and a total calorie count for that dish
       # And I see the chef's name.
       visit "/dishes/#{@dish1.id}"
+
       expect(page).to have_content(@dish1.name)
       expect(page).to have_content(@dish1.description)
+
+      within("#ingredient-list") do
+        expect(page).to have_content(@ingredient1.name)
+        expect(page).to have_content(@ingredient2.name)
+        expect(page).to have_content(@ingredient3.name)
+        expect(page).to have_content(@ingredient4.name)
+        expect(page).to_not have_content(@ingredient5.name)
+        expect(page).to_not have_content(@ingredient6.name)
+      end
+
+      expect(page).to have_content("Total Calories: 100")
+      expect(page).to have_content(@chef1.name)
     end
   end
 end
