1b095e18dc33540549aba87be2f764e4d31b7792
Justin Lee <140270202+JustinSteel@users.noreply.github.com>
2023-11-07 17:23:25 -0700

    make before each for test

--- spec/features/dishes/show_spec.rb
+++ spec/features/dishes/show_spec.rb
@@ -2,36 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'Dish Show Page' do
   describe 'As a visitor' do
+    before :each do
+    @chef_1 = Chef.create!(name: "Chef Mike")
+    @chef_2 = Chef.create!(name: "Chef John")
+    @chef_3 = Chef.create!(name: "Chef Bob")
+
+    @dish_1 = Dish.create!(name: "Spaghetti", description: "Spaghetti and Meatballs", chef_id: @chef_1.id)
+    @dish_2 = Dish.create!(name: "Pizza", description: "Pepperoni Pizza", chef_id: @chef_2.id)
+    @dish_3 = Dish.create!(name: "Burger", description: "Cheeseburger", chef_id: @chef_3.id)
+
+    @ingredient_1 = Ingredient.create!(name: "Spaghetti Noodles", calories: 200)
+    @ingredient_2 = Ingredient.create!(name: "Marinara Sauce", calories: 100)
+    @ingredient_3 = Ingredient.create!(name: "Meatballs", calories: 150)
+    @ingredient_4 = Ingredient.create!(name: "Pizza Dough", calories: 300)
+    @ingredient_5 = Ingredient.create!(name: "Pepperoni", calories: 200)
+    @ingredient_6 = Ingredient.create!(name: "Cheese", calories: 200)
+    @ingredient_7 = Ingredient.create!(name: "Bun", calories: 150)
+    @ingredient_8 = Ingredient.create!(name: "Beef Patty", calories: 250)
+    @ingredient_9 = Ingredient.create!(name: "Ketchup", calories: 100)
+    @ingredient_10 = Ingredient.create!(name: "Mustard", calories: 50)
+
+    @dish_1.ingredients << @ingredient_1
+    @dish_1.ingredients << @ingredient_2
+    @dish_1.ingredients << @ingredient_3
+    @dish_2.ingredients << @ingredient_4
+    @dish_2.ingredients << @ingredient_5
+    @dish_2.ingredients << @ingredient_6
+    @dish_3.ingredients << @ingredient_7
+    @dish_3.ingredients << @ingredient_8
+    @dish_3.ingredients << @ingredient_9
+    @dish_3.ingredients << @ingredient_10
+    end
     it 'I see the dish name and description' do
-      chef_1 = Chef.create!(name: "Chef Mike")
-      chef_2 = Chef.create!(name: "Chef John")
-      chef_3 = Chef.create!(name: "Chef Bob")
-
-      dish_1 = Dish.create!(name: "Spaghetti", description: "Spaghetti and Meatballs", chef_id: chef_1.id)
-      dish_2 = Dish.create!(name: "Pizza", description: "Pepperoni Pizza", chef_id: chef_2.id)
-      dish_3 = Dish.create!(name: "Burger", description: "Cheeseburger", chef_id: chef_3.id)
-
-      ingredient_1 = Ingredient.create!(name: "Spaghetti Noodles", calories: 200)
-      ingredient_2 = Ingredient.create!(name: "Marinara Sauce", calories: 100)
-      ingredient_3 = Ingredient.create!(name: "Meatballs", calories: 150)
-      ingredient_4 = Ingredient.create!(name: "Pizza Dough", calories: 300)
-      ingredient_5 = Ingredient.create!(name: "Pepperoni", calories: 200)
-      ingredient_6 = Ingredient.create!(name: "Cheese", calories: 200)
-      ingredient_7 = Ingredient.create!(name: "Bun", calories: 150)
-      ingredient_8 = Ingredient.create!(name: "Beef Patty", calories: 250)
-      ingredient_9 = Ingredient.create!(name: "Ketchup", calories: 100)
-      ingredient_10 = Ingredient.create!(name: "Mustard", calories: 50)
-
-      dish_1.ingredients << ingredient_1
-      dish_1.ingredients << ingredient_2
-      dish_1.ingredients << ingredient_3
-      dish_2.ingredients << ingredient_4
-      dish_2.ingredients << ingredient_5
-      dish_2.ingredients << ingredient_6
-      dish_3.ingredients << ingredient_7
-      dish_3.ingredients << ingredient_8
-      dish_3.ingredients << ingredient_9
-      dish_3.ingredients << ingredient_10
 
       
       # As a visitor
@@ -41,19 +43,19 @@ RSpec.describe 'Dish Show Page' do
       # and a total calorie count for that dish
       # And I see the chef's name.
       
-      visit "/dish/#{dish_1.id}"
+      visit "/dish/#{@dish_1.id}"
 
-      within("#dish-#{dish_1.id}") do
-        expect(page).to have_content(dish_1.name)
-        expect(page).to have_content(dish_1.description)
+      within("#dish-#{@dish_1.id}") do
+        expect(page).to have_content(@dish_1.name)
+        expect(page).to have_content(@dish_1.description)
         expect(page).to have_content(450)
       end
-        within("#ingredients-#{dish_1.id}") do
-          expect(page).to have_content(ingredient_1.name)
-          expect(page).to have_content(ingredient_2.name)
+        within("#ingredients-#{@dish_1.id}") do
+          expect(page).to have_content(@ingredient_1.name)
+          expect(page).to have_content(@ingredient_2.name)
         end
-          within("#chef-#{dish_1.id}") do
-            expect(page).to have_content(chef_1.name)        
+          within("#chef-#{@dish_1.id}") do
+            expect(page).to have_content(@chef_1.name)        
           end
     end
   end
