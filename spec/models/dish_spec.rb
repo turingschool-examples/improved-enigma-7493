df6183aaf8ae1751e3eba78e1f59fe123ab25df4
Charles Ren <cren04@syr.edu>
2023-11-07 19:12:55 -0800

    US 2 - #total_calories method test

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -1,6 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Dish, type: :model do
+  before :each do
+    @chef1 = Chef.create(name: "Ratatouille")
+    @dish1 = @chef1.dishes.create(name: "Pasta", description: "Yummys")
+
+    @ingredient1 = Ingredient.create(name: "Cheese", calorie: 20)
+    @ingredient2 = Ingredient.create(name: "Pasta", calorie: 10)
+    @ingredient3 = Ingredient.create(name: "Sauce", calorie: 5)
+
+    @dish1.ingredients << @ingredient1
+    @dish1.ingredients << @ingredient2
+    @dish1.ingredients << @ingredient3
+  end
+
   describe "validations" do
       it {should validate_presence_of :name}
       it {should validate_presence_of :description}
@@ -10,5 +23,13 @@ RSpec.describe Dish, type: :model do
       it {should belong_to :chef}
       it {should have_many(:dish_ingredients)}
       it {should have_many(:ingredients).through(:dish_ingredients)}
+  end
+
+  describe "instance_methods" do
+    describe "#total_calories" do
+      it "can sum the total calories in a dish" do
+        expect(@dish1.total_calories).to eq(35)
+      end
+    end
   end
 end
